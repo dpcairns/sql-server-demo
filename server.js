90879525f9463952888639cd532435937370e9a0
Danny Cairns <dpcairns@gmail.com>
2020-02-20 12:29:33 -0800

    okay

---
+++ server.js
@@ -0,0 +1,50 @@
+// Load Environment Variables from the .env file
+require("dotenv").config();
+
+// Application Dependencies
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const pg = require("pg");
+
+console.log(process.env);
+// Database Client
+const Client = pg.Client;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
+// Application Setup
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan("dev")); // http logging
+app.use(cors()); // enable CORS request
+
+// API Routes
+
+app.get("/api/cats", async (req, res) => {
+  try {
+    const result = await client.query(`
+            SELECT
+                id,
+                name,
+                type,
+                url,
+                year,
+                is_sidekick as "isSidekick"
+            FROM CATS;
+        `);
+
+    console.log(result.rows);
+
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message || err
+    });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("server running on PORT", PORT);
+});
